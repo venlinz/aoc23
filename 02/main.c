0475e01f3c78276f5ec995a8270062b452c0350d
Venkatesh.T <shariharanvenkatesh@gmail.com>
2024-01-01 17:52:03 +0530

    Add solution for part 2 of Day 02
    Refactor:
    1) read file to heap
    2) introduce type for Cubessets

--- 02/main.c
+++ 02/main.c
@@ -8,35 +8,49 @@
 
 char * lines(char **string);
 void chop_by_delim(char **string, char *delim);
-char * get_input(const char *filepath);
-char ** tokenize_by_delim(char **string, char *delim, char **tokens);
+char * read_file_as_string(const char *filepath);
+char ** tokenize_by_delims(char **string, char *delim, char **tokens);
+
+typedef struct {
+    char *color;
+    int count;
+    int color_code;
+} CubeSet;
 
 bool check_is_game_possible(char *line);
-bool validate_color_count(char *color, char *count_str);
-
+bool validate_color_count(CubeSet cube);
+int get_power_of_minimum_cubes(char *line);
+CubeSet get_color(char *cube_str);
 
 #define MAX_RED_CUBES 12
 #define MAX_GREEN_CUBES 13
 #define MAX_BLUE_CUBES 14 
 
+#define COLOR_RED "red"
+#define COLOR_GREEN "green"
+#define COLOR_BLUE "blue"
+
+#define COLOR_CODE_RED 1
+#define COLOR_CODE_GREEN 2
+#define COLOR_CODE_BLUE 3
 
 int part1(char *input);
 int part2(char *input);
 
-#define MAX_FILE_SIZE (24 * 1024)
-char input[MAX_FILE_SIZE] = {0};
-
 int main(void) {
     const char *filepath = "./input.txt";
-    char *input = get_input(filepath);
+    char *input = read_file_as_string(filepath);
     if (input == NULL)
     {
         return 1;
     }
+    char *input_copy = strdup(input);
     int sum1 = part1(input);
     printf("part1: total sum: %i\n", sum1);
-    // int sum2 = part2(input);
-    // printf("part2: total sum: %i\n", sum2);
+    int sum2 = part2(input_copy);
+    free(input);
+    free(input_copy);
+    printf("part2: total sum: %i\n", sum2);
     return 0;
 }
 
@@ -54,65 +68,82 @@ int part1(char *input) {
     return sumation_of_possible_game_nos;
 }
 
-// int part2(char *input) {
-//     return 0;
-// }
-
-char * get_input(const char *filepath) {
-    FILE *f = fopen(filepath, "r");
-    if (f == NULL) {
-        perror("ERROR");
-        return NULL;
+int part2(char *input) {
+    char *line = NULL;
+    int sum_of_power_of_minimum_cubes = 0;
+    while ((line = lines(&input)) != NULL) {
+        int power_of_minimum_cubes = get_power_of_minimum_cubes(line);
+        sum_of_power_of_minimum_cubes += power_of_minimum_cubes;
     }
-    int ret = fseek(f, 0, SEEK_END);
-    assert(ret == 0 && "fseek failed");
-    size_t filesize = ftell(f);
-    assert(filesize <= MAX_FILE_SIZE);
-    rewind(f);
-    ret = fread(input, filesize, 1, f);
-    // assert(ret == 1);
-    if (!feof(f) && ferror(f)) {
-        perror("ERROR: ");
-        return NULL;
-    }
-    fclose(f);
-    return input;
+    return sum_of_power_of_minimum_cubes;
 }
 
+
 bool check_is_game_possible(char *line) {
-    // printf("%s\n", line);
     chop_by_delim(&line, ":");
     char *games[16] = {0};
-    tokenize_by_delim(&line, ";", (char **) games);
+    tokenize_by_delims(&line, ";", (char **) games);
     bool is_game_possible = true;
     for (int i = 0; games[i] != NULL; ++i) {
-        // printf("  cube set: %s\n", games[i]);
         char *cubes[6] = {0};
-        tokenize_by_delim(&games[i], ",", (char **) cubes);
+        tokenize_by_delims(&games[i], ",", (char **) cubes);
         for (int i = 0; cubes[i] != NULL; ++i) {
-            cubes[i] = trim(&cubes[i]);
-            // printf("    cube: %s\n", cubes[i]);
-            char *tuple[2] = {0};
-            tokenize_by_delim(&cubes[i], " ", (char **)tuple);
-            char *cube_count = tuple[0];
-            char *cube_color = tuple[1];
-            // printf("      cube color: %s, cube count: %s\n", cube_color, cube_count);
-            bool is_valid = validate_color_count(cube_color, cube_count);
+            CubeSet single_cube_set = get_color(trim(&cubes[i]));
+            bool is_valid = validate_color_count(single_cube_set);
             is_game_possible &= is_valid;
         }
     }
     return is_game_possible;
 }
 
-bool validate_color_count(char *color, char *count_str) {
-    long count = strtol(count_str, NULL, 10);
-    if (strncmp(color,  "red", 3) == 0) {
+CubeSet get_color(char *cube_str) {
+    char *tuple[2] = {0};
+    tokenize_by_delims(&cube_str, " ", tuple);
+    long count = strtol(tuple[0], NULL, 10);
+
+    int color_code = 0;
+    if (strncmp(tuple[1], COLOR_RED, sizeof(COLOR_RED)) == 0) {
+        color_code = COLOR_CODE_RED;
+    } else if (strncmp(tuple[1], COLOR_BLUE, sizeof(COLOR_BLUE)) == 0) {
+        color_code = COLOR_CODE_BLUE;
+    } else if (strncmp(tuple[1], COLOR_GREEN, sizeof(COLOR_GREEN)) == 0) {
+        color_code = COLOR_CODE_GREEN;
+    } else { assert(false && "Unreachable"); }
+
+    CubeSet cubes = {.color = tuple[1], .count = count, .color_code = color_code};
+    return cubes;
+}
+
+int get_power_of_minimum_cubes(char *line) {
+    int min_red_cubes = 0;
+    int min_blue_cubes = 0;
+    int min_green_cubes = 0;
+    chop_by_delim(&line, ":");
+    char *multiple_cubesets[32] = {0};
+    tokenize_by_delims(&line, ",;", multiple_cubesets);
+    for (int i = 0; multiple_cubesets[i] != NULL; ++i) {
+        CubeSet single_cube_set = get_color(trim(&multiple_cubesets[i]));
+        int color_code = single_cube_set.color_code;
+        int cubes_count = single_cube_set.count;
+        if (color_code == COLOR_CODE_RED && min_red_cubes <= cubes_count) {
+            min_red_cubes = cubes_count;
+        } else if (color_code == COLOR_CODE_BLUE && min_blue_cubes <= cubes_count) {
+            min_blue_cubes = cubes_count;
+        } else if (color_code == COLOR_CODE_GREEN && min_green_cubes <= cubes_count) {
+            min_green_cubes = cubes_count;
+        }
+    }
+    return min_blue_cubes * min_red_cubes * min_green_cubes;
+}
+
+bool validate_color_count(CubeSet cubes) {
+    int color_code = cubes.color_code;
+    int count = cubes.count;
+    if (color_code == COLOR_CODE_RED) {
         return MAX_RED_CUBES >= count;
-    }
-    if (strncmp(color,  "blue", 4) == 0) {
+    } else if (color_code == COLOR_CODE_BLUE) {
         return MAX_BLUE_CUBES >= count;
-    }
-    if (strncmp(color,  "green", 5) == 0) {
+    } else if (color_code == COLOR_CODE_GREEN) {
         return MAX_GREEN_CUBES >= count;
     }
     return false;
@@ -132,10 +163,34 @@ void chop_by_delim(char **string, char *delim) {
     strsep(string, delim);
 }
 
-char ** tokenize_by_delim(char **string, char *delim, char **tokens) {
+char ** tokenize_by_delims(char **string, char *delim, char **tokens) {
     char *token = NULL;
     for (size_t i = 0; (token = strsep(string, delim)) != NULL; ++i) {
         tokens[i] = token;
     }
     return tokens;
+}
+
+char * read_file_as_string(const char *filepath) {
+    FILE *f = fopen(filepath, "r");
+    if (f == NULL) {
+        perror("ERROR");
+        return NULL;
+    }
+    int ret = fseek(f, 0, SEEK_END);
+    assert(ret == 0 && "fseek failed");
+    size_t filesize = ftell(f);
+    rewind(f);
+    char *input = malloc(filesize + 1);
+    if (input == NULL) {
+        fprintf(stderr, "ERROR: Unable to alloc memmory");
+        return NULL;
+    }
+    ret = fread(input, filesize, 1, f);
+    if (!feof(f) && ferror(f)) {
+        perror("ERROR: ");
+        return NULL;
+    }
+    fclose(f);
+    return input;
 }
